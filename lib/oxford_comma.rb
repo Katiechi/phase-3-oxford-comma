fb8840140e274cd930e77fc2a4878f7dbfcd1d46
brian ambundo <brian.katiechi9@gmail.com>
2022-08-18 19:01:25 +0300

    commit tree fa827f1172edad5c1a6a2c44c4fee7fbb4c9cc37

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
-end
+    if array.length == 2
+        new_list= array.join(", ")
+        new_list.reverse.sub(',', 'dna ').reverse
+    else
+        new_list =   array.join(", ")
+        new_list.reverse.sub(',', 'dna ,').reverse
+    end
+  end
+  puts oxford_comma(["fiddleheads", "okra", "kohlrabi"])
